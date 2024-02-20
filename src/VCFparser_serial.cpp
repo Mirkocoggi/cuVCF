cd9bcb48e7dcf5065342ee63e01cca3a33190aee
Mirkocoggi <mirko1998coggi@gmail.com>
2024-02-20 15:11:45 +0100

    first serial vcf parser using string

--- src/VCFparser_serial.cpp
+++ src/VCFparser_serial.cpp
@@ -9,11 +9,46 @@
 #include <zlib.h>
 #include <queue>
 #include <chrono>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/predicate.hpp>
 #include "VCFparser_serial_struct.h"
 
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
+   
+    int opt;
+    char *vcf_filename; 
+   
+    while ((opt = getopt(argc, argv, "v:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'v':
+            vcf_filename = optarg;
+            cout << "vcf_filename: " << vcf_filename << endl;
+            break;
+        case '?':
+            cout << "Unknown option: " << optopt << endl;
+            break;
+        }
+    }
 
+    string filename = vcf_filename;
+    ifstream inFile(filename);
+    if(!inFile){
+        cout << "ERROR: cannot open file " << filename << endl;
+        return -1;
+    }
+    int cont=0;
+    string line;
+    vcf_parsed vcf;
+    vcf.get_filename(filename);
+    vcf.get_header(&inFile);
+    //vcf.print_header();
+    vcf.create_var_struct(&inFile);
+    
+    
+    
     return 0;
 }
--- src/VCFparser_serial_struct.h
+++ src/VCFparser_serial_struct.h
@@ -1,6 +1,7 @@
 #ifndef VCF_STRUCTS_H
 #define VCF_STRUCTS_H
 #include <chrono>
+#include <boost/algorithm/string.hpp>
 
 
 using namespace std;
@@ -8,9 +9,10 @@ using namespace std;
 class var
 {
 public:
+    int var_number;
 	string chrom;
     int pos;
-    int id;
+    string id;
     string ref;
     string alt;
     int qual;
@@ -18,6 +20,98 @@ public:
     string info;
     string format;
     vector<string> samples;
+    void get_vcf_line(string line)
+    {
+        vector<string> line_el;
+        boost::split(line_el, line, boost::is_any_of("\t "));
+        chrom = line_el[0];
+        pos = stoi(line_el[1]);
+        id = line_el[2];
+        ref = line_el[3];
+        alt = line_el[4];
+        qual = stoi(line_el[5]);
+        filter = line_el[6];
+        info = line_el[7];
+        format = line_el[8];
+        string samples_string = line_el[9];
+        line_el.clear();
+        boost::split(line_el, samples_string, boost::is_any_of("\t "));
+        for (int i=0; i<line_el.size(); i++){
+            samples.push_back(line_el[i]);
+        }
+    }
+    void print_var()
+    {
+        cout << "Var " << var_number << " : ";
+        cout << chrom << "\t";
+        cout << to_string(pos) << "\t";
+        cout << id << "\t";
+        cout << ref << "\t";
+        cout << alt << "\t";
+        cout << to_string(qual) << "\t";
+        cout << filter << "\t";
+        cout << info << "\t";
+        cout << format << "\t";
+        for (int i = 0; i < samples.size(); i++)
+        {
+            cout << samples[i] << "\t";
+        }
+        cout << endl;
+    }
+};
+
+class vcf_parsed
+{
+public:
+    int id;
+    string filename;
+    vector<var> var_df;
+    string header;
+    int num_var = 0;
+    void get_filename(string path_to_filename)
+    {
+        vector<string> line_el;
+        boost::split(line_el, path_to_filename, boost::is_any_of("/"));
+        filename = line_el[line_el.size()-1];
+        cout << "\nOnly file name: " << filename << endl;
+        line_el.clear();
+        boost::split(line_el, filename, boost::is_any_of("."));
+        if(line_el[line_el.size()-1] == "gz")
+        {
+            cout << "\nFile must be uncompressed!\n" << endl;
+            exit(0);
+        }else{
+            cout << "\nFile already uncompressed!\n" << endl;
+        }
+    }
+    void get_header(ifstream *file)
+    {
+        string line;
+        //removing the header and storing it in vcf.header
+        while (getline(*file, line) && line[0]=='#' && line[1]=='#'){
+            header.append(line + '\n');
+        }
+        cout << "\nLast line: "<< line<< endl;
+        
+    }
+    void print_header()
+    {
+        cout << "VCF header:\n" << header << endl;
+    }
+    void create_var_struct(ifstream *file)
+    {
+        string line;
+        var var_tmp;
+        while (getline(*file, line))
+        {
+            var_tmp.get_vcf_line(line);
+            num_var++;
+            var_tmp.var_number = num_var;
+            var_tmp.print_var();
+            var_df.push_back(var_tmp);
+            var_tmp.samples.clear();
+        }
+    }
 };
 
 #endif
