d75854ba59087f48423da74a66d2f46a4e67c003
Mirkocoggi <mirko1998coggi@gmail.com>
2024-03-28 14:56:25 +0100

    added code for var columns and modified Makefile

--- Makefile
+++ Makefile
@@ -3,13 +3,19 @@ CPPFLAGS = -O3 -std=c++17
 #CPPFLAGS = -std=c++17
 LIBS = -lz
 
-S2GA:
+VARSTRUCT:
 	mkdir -p bin/
 	$(CXX) src/VCFparser_mt.cpp -o bin/VCFparser -fopenmp $(CPPFLAGS) $(LIBS)
 #	$(CXX) src/VCFparser_mt.cpp -o bin/VCFparser -fopenmp $(CPPFLAGS) $(LIBS) -g
 #poi lancialo con gcc --args
+VARCOL:
+	mkdir -p bin/
+	$(CXX) src/VCFparser_mt_col.cpp -o bin/VCFparser -fopenmp $(CPPFLAGS) $(LIBS)
+#	$(CXX) src/VCFparser_mt.cpp -o bin/VCFparser -fopenmp $(CPPFLAGS) $(LIBS) -g
+#poi lancialo con gcc --args
 all:
-	$(S2GA)
+	$(VARSTRUCT)
+	$(VARCOL)
 
 clean:
 	rm -rf bin/
---
+++ src/VCFparser_mt_col_struct.h
@@ -0,0 +1,511 @@
+#ifndef VCF_STRUCTS_H
+#define VCF_STRUCTS_H
+#include <chrono>
+#include <boost/algorithm/string.hpp>
+
+
+using namespace std;
+
+class var_columns_df
+{
+public:
+    vector<long> var_number;
+    vector<string> chrom;
+    vector<long>pos;
+    vector<string> id;
+    vector<string> ref;
+    vector<string> alt;
+    vector<float> qual;
+    vector<string> filter;
+    vector<string> info;
+    void get_vcf_line_in_var_columns(char *line, long start, long end, long i)
+    {
+        bool find1 = false;
+        long iter=0;
+        string tmp="\0";
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+                chrom[i] = tmp;
+            }else{
+                tmp += line[start+iter];
+                iter++;
+            }
+        }
+        tmp="\0";
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+                pos[i] = stoul(tmp);
+            }else{
+                tmp += line[start+iter];
+                iter++;
+            }
+        }
+        tmp="\0";
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+                id[i] = tmp;
+            }else{
+                tmp += line[start+iter];
+                iter++;
+            }
+        }
+        tmp="\0";
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+                ref[i] = tmp;
+            }else{
+                tmp += line[start+iter];
+                iter++;
+            }
+        }
+        tmp="\0";
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+                alt[i] = tmp;
+            }else{
+                tmp += line[start+iter]; // per ora le salvo tutte come un unico array of char, andrebbe cambiato per salvarle separatamente
+                iter++;
+            }
+        }
+        tmp="\0";
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+                if(strcmp(&tmp[0], ".")==0){
+                    qual[i] = 0.0f;
+                }else{
+                    qual[i] = stof(tmp); // da cambiare, in futuro
+                }
+            }else{
+                tmp += line[start+iter];
+                iter++;
+            }
+        }
+        tmp="\0";
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+                filter[i] = tmp;
+            }else{
+                tmp += line[start+iter];
+                iter++;
+            }
+        }
+        tmp="\0";
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '||line[start+iter]=='\n'){
+                find1 = true;
+                iter++;
+                info[i] = tmp;
+            }else{
+                tmp += line[start+iter]; // anche qui andrebbero separate le info e creati dizionari con keys e values
+                iter++;
+                
+            }
+        }
+        
+    }
+    void print_var_columns(long num_lines){
+        for(long i=0; i<num_lines; i++){
+            cout << "Var" << var_number[i] << ":\t";
+            cout << chrom[i] << "\t";
+            cout << to_string(pos[i]) << "\t";
+            cout << id[i] << "\t";
+            cout << ref[i] << "\t";
+            cout << alt[i] << "\t";
+            cout << to_string(qual[i]) << "\t";
+            cout << filter[i] << "\t";
+            cout << info[i] << endl;
+        }
+    }    
+};
+
+class var
+{
+public:
+    long var_number;
+	string chrom="\0"; // su quale cromosoma della reference
+    long pos; // posizione nel chromosoma
+    string id="\0"; // id della variation, spesso .
+    string ref="\0"; // nucleotide della reference
+    string alt="\0"; // possibili alternative (possono essere più di una)
+    float qual; // quality score
+    string filter="\0"; // filtro penso usato durante il sequenziamento
+    string info="\0"; // info varie deducibili dall'header, potrebbe essere utile averle in collegamento
+    string format="\0"; // formato dei samples, info variabiliti, mi dice come sono ordinate
+    string samples="\0"; // una colonna per ogni sample in cui ognuno descrive i valori indicati in format
+    void get_vcf_line(char *line, long start, long end)
+    {
+        bool find1 = false;
+        long iter=0;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+            }else{
+                chrom += line[start+iter];
+                iter++;
+            }
+        }
+        string tmp="\0";
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+                pos = stoul(tmp); // da cambiare, in futuro
+            }else{
+                tmp += line[start+iter];
+                iter++;
+            }
+        }
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+            }else{
+                id += line[start+iter];
+                iter++;
+            }
+        }
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+            }else{
+                ref += line[start+iter];
+                iter++;
+            }
+        }
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+            }else{
+                alt += line[start+iter]; // per ora le salvo tutte come un unico array of char, andrebbe cambiato per salvarle separatamente
+                iter++;
+            }
+        }
+        tmp="\0";
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+                if(strcmp(&tmp[0], ".")==0){
+                    qual = 0.0;
+                }else{
+                    qual = stof(tmp); // da cambiare, in futuro
+                }
+            }else{
+                tmp += line[start+iter];
+                iter++;
+            }
+        }
+        find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '){
+                find1 = true;
+                iter++;
+            }else{
+                filter += line[start+iter];
+                iter++;
+            }
+        }
+        find1=false;
+        while(!find1 && (start+iter)<(end-1)){
+            if(line[start+iter]=='\t'||line[start+iter]==' '||line[start+iter]=='\n'){
+                find1 = true;
+                iter++;
+            }else{
+                info += line[start+iter]; // anche qui andrebbero separate le info e creati dizionari con keys e values
+                iter++;
+            }
+        }
+        /*find1=false;
+        while(!find1){
+            if(line[start+iter]=='\t'||line[start+iter]==' '||line[start+iter]=='\n'){
+                find1 = true;
+                iter++;
+            }else{
+                format += line[start+iter]; // vanno separati e soprattutti connessi ai valori nei samples (tipo se nel format ho ..:AF e nel sample ..:2, devo poter fare la query AF=2 in tutte le var)
+                iter++;
+            }
+        }
+        
+        while((start+iter)<(end-1)){ //segmentation fault -> perchè non (start+iter)<(end-1)?
+            samples += line[start+iter]; // sicuramente da dividere i vari sample e eventualmente creare un dizionario per ogni sample cosi da associare con il format
+            iter++;
+        }*/
+        
+    }
+    void print_var()
+    {
+        cout << "Var" << var_number << ":\t";
+        cout << chrom << "\t";
+        cout << to_string(pos) << "\t";
+        cout << id << "\t";
+        cout << ref << "\t";
+        cout << alt << "\t";
+        cout << to_string(qual) << "\t";
+        cout << filter << "\t";
+        cout << info << endl;
+        //cout << format << "\t";
+        //cout << samples;
+        //cout << endl;
+    }
+};
+
+class header_element
+{
+public:
+    vector<string> ID;
+    vector<string> Number;
+    vector<string> Type;
+    int total_values=0;
+    int alt_values=0;
+    int no_alt_values=0;
+};
+
+class vcf_parsed
+{
+public:
+    int id;
+    string filename;
+    var *var_df;
+    string header;
+    header_element INFO;
+    header_element FORMAT; //aggiungere Filter e gli altri, magari con switch case
+    char *filestring;
+    int header_size=0;
+    long filesize;
+    long variants_size;
+    long num_lines=0;
+    long *new_lines_index;
+    var_columns_df var_columns;
+    void get_filename(string path_to_filename){
+        vector<string> line_el;
+        boost::split(line_el, path_to_filename, boost::is_any_of("/"));
+        filename = line_el[line_el.size()-1];
+        //cout << "\nOnly file name: " << filename << endl;
+        line_el.clear();
+        boost::split(line_el, filename, boost::is_any_of("."));
+        if(line_el[line_el.size()-1] == "gz")
+        {
+            //cout << "\nFile must be uncompressed!\n" << endl;
+            exit(0);
+        }else{
+            //cout << "\nFile already uncompressed!\n" << endl;
+        }
+    }
+    void get_file_size(string filename){
+        filesize = filesystem::file_size(filename);
+    }
+    void get_header(ifstream *file){
+        string line;
+        //removing the header and storing it in vcf.header
+        while (getline(*file, line) && line[0]=='#' && line[1]=='#'){
+            header.append(line + '\n');
+            header_size += line.length() + 1;
+        }
+        header_size += line.length() + 1;
+        //cout << "\nheader char: " << to_string(header_size) << endl;
+        variants_size = filesize - header_size; //ora che ho tolto l'header ho un file piu piccolo quindi una nuova size
+        //cout<<"filesize: "<<filesize<<" variants_size: "<<variants_size<<endl;
+    }
+    void print_header(){
+        //cout << "VCF header:\n" << header << endl;
+    }
+    void get_and_parse_header(ifstream *file){
+        string line;
+        vector<string> line_el; //all the characteristics together
+        vector<string> line_el1; //each characteristic
+        vector<string> line_el2; //keys and values
+        //removing the header and storing it in vcf.header
+        
+        while (getline(*file, line) && line[0]=='#' && line[1]=='#'){
+            header.append(line + '\n');
+            header_size += line.length() + 1;
+            bool Info = (line[2]=='I');
+            bool Format = (line[2]=='F' && line[3]=='O');
+            
+            if(Info || Format){
+                boost::split(line_el, line, boost::is_any_of("><"));
+                boost::split(line_el1, line_el[1], boost::is_any_of(","));
+                for(int i=0; i<3; i++){
+                    boost::split(line_el2, line_el1[i], boost::is_any_of("="));
+                    if(Info){
+                        if(i==0) INFO.ID.push_back(line_el2[1]);
+                        if(i==1) INFO.Number.push_back(line_el2[1]);
+                        if(i==1 && line_el2[1] == "A") INFO.alt_values++;
+                        if(i==2) INFO.Type.push_back(line_el2[1]);
+                    }
+                    if(Format){
+                        if(i==0) FORMAT.ID.push_back(line_el2[1]);
+                        if(i==1) FORMAT.Number.push_back(line_el2[1]);
+                        if(i==2) FORMAT.Type.push_back(line_el2[1]);
+                    }
+                }
+            }
+        }
+        for(int i=0; i<INFO.ID.size(); i++){
+            cout<<"INFO.ID["<<i<<"]: "<<INFO.ID[i]<<" | INFO.Number["<<i<<"]: "<<INFO.Number[i]<<" | INFO.Type["<<i<<"]: "<<INFO.Type[i]<<endl;
+        }
+        INFO.total_values = INFO.ID.size();
+        INFO.no_alt_values = INFO.total_values - INFO.alt_values;
+        cout<<"INFO.total_values: "<< INFO.total_values << " INFO.alt_values: "<<INFO.alt_values<<" INFO.no_alt_values: "<<INFO.alt_values<<endl;
+        header_size += line.length() + 1;
+        cout << "\nheader char: " << to_string(header_size) << endl;
+        variants_size = filesize - header_size; //ora che ho tolto l'header ho un file piu piccolo quindi una nuova size
+        cout<<"filesize: "<<filesize<<" variants_size: "<<variants_size<<endl;
+    }
+    void allocate_filestring(){
+        filestring = (char*)malloc(variants_size);
+    }
+    void find_new_lines_index(string w_filename, int num_threads){ //popola anche il filestring
+        long num_char=0;
+        new_lines_index = (long*)malloc(variants_size); //per ora ho esagerato con la dimensione (è come se permettessi tutti \n. Si puo ridurre (euristicamente), pero ipotizzarlo è meglio perche senno devo passare il file due volte solo per vedere dove iniziano le linee)
+        new_lines_index[0] = 0; //il primo elemento lo metto a zero per indicare l'inizio della prima linea
+        num_lines++;
+        auto before = chrono::system_clock::now();
+        long batch_infile = (variants_size - 1 + num_threads)/num_threads; //numero di char che verrà processato da ogni thread
+
+        /*-----------------------------------------------------
+        std::ifstream file_streams[num_threads]; //std::vector<std::ifstream> file_streams(num_threads); // std::ifstream file_streams[num_threads] ??
+        for (int i = 0; i < num_threads; i++) {
+            // Set the offset for this thread
+            int start = header_size + i*batch_infile;
+            file_streams[i].open(w_filename); // Open the file stream for this thread
+            file_streams[i].seekg(start, ios::cur); // Set the file stream's read position to the start offset
+        }
+        */        
+#pragma omp parallel
+        {
+            int thr_ID = omp_get_thread_num();
+            ifstream infile(w_filename); //apro lo stesso file con un ifstream in ogni thread, da considerare se ha senso creare prima un array di ifstream
+            infile.seekg((header_size + thr_ID*batch_infile), ios::cur); //ogni thread parte a copiare inFile in filestring a una distanza di batch_size
+            long start, end;
+            start = thr_ID*batch_infile; // inizio del batch dello specifico thread
+            end = start + batch_infile; // fine del batch
+            //cout << "in find start: "<<start<<" end: "<<end<<endl;
+            for(long i=start; i<end && i<variants_size; i++){
+                filestring[i] = infile.get(); //file_streams[thr_ID].get(); //-----------------
+            }
+        }
+        while(filestring[variants_size-1]=='\n'){
+            variants_size--;
+        }
+        filestring[variants_size] = '\n';
+        variants_size++;
+        auto after = chrono::system_clock::now();
+        auto filestring_time = std::chrono::duration<double>(after - before).count();
+        before = chrono::system_clock
+        ::now();
+        num_char = 0;
+        while(num_char!=variants_size){ //conto il numero di lines e ogni volta che trovo \n salvo il char successivo come inizio della riga successiva
+            if(filestring[num_char]=='\n'&& filestring[num_char+1]!='\n'){
+                new_lines_index[num_lines] = num_char+1; // PROBLEMA: funziona solo se l'ultimo char è uno /n, altrimenti si rompe
+                num_lines++;
+            }
+            num_char++;
+        } // si potrebbe fare multithreading ma vediamo se si ha beneficio
+        after = chrono::system_clock::now();
+
+        auto f_new_lines = std::chrono::duration<double>(after - before).count(); 
+        //cout << /*"\nFilestring time: " <<*/ filestring_time /*<< " s " << "New lines time: " << f_new_lines << " s\n\n"*/ << endl; //da printare
+    }
+    void reserve_var_columns(){
+        var_columns.var_number.resize(num_lines-1);
+        var_columns.chrom.resize(num_lines-1);
+        var_columns.id.resize(num_lines-1);
+        var_columns.pos.resize(num_lines-1);
+        var_columns.ref.resize(num_lines-1);
+        var_columns.alt.resize(num_lines-1);
+        var_columns.qual.resize(num_lines-1);
+        var_columns.filter.resize(num_lines-1);
+        var_columns.info.resize(num_lines-1);
+        cout<<"Finish resize!"<<endl;
+    }
+    void populate_var_columns(int num_threads){
+        long batch_size = (num_lines-2+num_threads)/num_threads;
+
+#pragma omp parallel
+        {
+            long start, end;
+            int th_ID = omp_get_thread_num();
+            //cout << "\nThread id: "<<th_ID<<endl;
+
+            start = th_ID*batch_size; // inizio del batch dello specifico thread
+            end = start + batch_size; // fine del batch
+
+            for(long i=start; i<end && i<num_lines-1; i++){ //start e end mi dicono l'intervallo di linee che eseguirà ogni thread, quindi la i rappresenta l'iesima linea (var) che inizia a new_lines_index[i] e finisce a new_lines_index[i+1] (escluso)
+                var_columns.var_number[i] = i;
+                //cout<<"Var_number[i]: "<<var_columns.var_number[i]<<endl;
+                var_columns.get_vcf_line_in_var_columns(filestring, new_lines_index[i], new_lines_index[i+1], i);
+                //cout<<"enf"<<endl;
+            }
+
+        }
+    }
+
+
+
+
+
+
+    void populate_var_struct(int num_threads){
+        auto before = chrono::system_clock::now();
+        
+        var_df = (var*)calloc((num_lines-1), sizeof(var)); // allocating var_df    
+        //cout << "\nBegin tmp: \n" <<"newlines: "<<num_lines<<" num threads: "<<num_threads<<endl;
+        long batch_size = (num_lines-2+num_threads)/num_threads; //numero di lines che verrà processato da ogni thread
+        
+        //cout << "\nBatch size: "<<batch_size<<endl;
+        auto after = chrono::system_clock::now();
+        auto pre_pragma = std::chrono::duration<double>(after - before).count();
+        //cout << "Pre_pragma: " << pre_pragma << " s" << endl;
+
+#pragma omp parallel
+        {
+            long start, end;
+            int th_ID = omp_get_thread_num();
+            //cout << "\nThread id: "<<th_ID<<endl;
+
+            start = th_ID*batch_size; // inizio del batch dello specifico thread
+            end = start + batch_size; // fine del batch
+
+            //cout << "\nstart: " << start << " end: " << end << endl; //segfault
+
+            for(long i=start; i<end && i<num_lines-1; i++){ //start e end mi dicono l'intervallo di linee che eseguirà ogni thread, quindi la i rappresenta l'iesima linea (var) che inizia a new_lines_index[i] e finisce a new_lines_index[i+1] (escluso)
+                var_df[i].get_vcf_line(filestring, new_lines_index[i], new_lines_index[i+1]); //qui traduco da char* a var structure la specifica line
+                var_df[i].var_number = i; // dato aggiuntivo come se fosse un altro ID interno 
+            }
+
+        }
+    }
+
+};
+
+#endif
